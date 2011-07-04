467dac21939cbe7015944a9dcd483d5954ab9286
Fabio Comuni <fabrix.xm@gmail.com>
2011-07-04 09:20:23 +0200

    Update to Thunderbird 5 / Lightning 1.0b4

---
+++ chrome/content/calendaryearview-common-sets.js
@@ -0,0 +1,26 @@
+﻿//
+
+calendarController.commands["calendar_year-view_command"] = true;
+
+calendarController._doCommand = calendarController.doCommand;
+calendarController.doCommand = function(aCommand){
+	switch (aCommand) {
+		case "calendar_year-view_command":
+			switchCalendarView("year", true);
+			break;		
+		default:
+			return this._doCommand(aCommand);
+	}
+}
+
+
+calendarController._isCommandEnabled = calendarController.isCommandEnabled;
+calendarController.isCommandEnabled = function(aCommand) {
+	switch (aCommand) {
+		case "calendar_year-view_command":
+			return this.isInMode("calendar");		
+		default:
+			return this._isCommandEnabled(aCommand);
+	}
+
+}
